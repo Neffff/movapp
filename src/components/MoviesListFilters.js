6d77b5f917be5b261822906d056e15327832dc4c
Neffff <saymek@o2.pl>
2018-03-06 16:53:14 +0100

    fire onchange input after end writing

--- src/components/MoviesListFilters.js
+++ src/components/MoviesListFilters.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux';
 import {searchMovieList} from '../actions/actions';
 import FaSearch from 'react-icons/lib/fa/search';
 import '../styles/moviesListFilters.css';
 
-const MoviesListFilters = (props) => (
+const WAIT_INTERVAL = 500;
+let timer = null;
 
-    <div className="filter__container">
-        <div className="filter__input">
-            <FaSearch/>
-            <input
-                type="text"
-                onChange={(e) => {
-                props.dispatch(searchMovieList(e.target.value))
-            }}/>
+class MoviesListFilters extends Component {
+    render() {
+        const {dispatch} = this.props;
+        return (
+            <div className="filter__container">
+                <div className="filter__input">
+                    <FaSearch/>
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                        let val = e.target.value;
+                        clearTimeout(timer);
+                        timer = setTimeout(() => this.props.dispatch(searchMovieList(val)), WAIT_INTERVAL);
+                    }}/>
 
-        </div>
-    </div>
-);
+                </div>
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state) => {
     return {filters: state.visibilityFilter};
